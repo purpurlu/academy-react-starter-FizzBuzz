e88b9a33a11f4784fb3e68445b1a94fb428f1f12
purpurlu <laurazurbayeva@gmail.com>
2022-07-24 18:05:19 +0100

    create FizzBuzz App

--- src/utils/greet.test.ts
+++ src/utils/greet.test.ts
@@ -1,7 +1,14 @@
-import { greet } from "./greet";
+// import { greet } from "./greet";
 
-test("greet returns a string, greeting the passed name", () => {
-  expect(greet("World")).toBe("Hello, World!");
-  expect(greet("Richard")).toBe("Hello, Richard!");
-  expect(greet("Academy Scholars")).toBe("Hello, Academy Scholars!");
+// test("greet returns a string, greeting the passed name", () => {
+//   expect(greet("World")).toBe("Hello, World!");
+//   expect(greet("Richard")).toBe("Hello, Richard!");
+//   expect(greet("Academy Scholars")).toBe("Hello, Academy Scholars!");
+// });
+
+import { asFizzBuzz } from "./greet";
+test("asFizzBuzz returns an array of numbers with strings", () => {
+  expect(asFizzBuzz(1)).toBe(1);
+  expect(asFizzBuzz(15)).toBe("FizzBuzz");
+  expect(asFizzBuzz(105)).toBe("FizzBuzz");
 });
--- src/utils/greet.ts
+++ src/utils/greet.ts
@@ -1,3 +1,33 @@
-export function greet(name: string): string {
-  return `Hello, ${name}!`;
+// export function greet(name: string): string {
+//   return `Hello, ${name}!`;
+// }
+
+function isFizz(num: number): boolean {
+  if (num % 3 === 0) {
+    return true;
+  }
+  return false;
+}
+function isBuzz(num: number): boolean {
+  if (num % 5 === 0) {
+    return true;
+  }
+  return false;
+}
+function isFizzBuzz(num: number): boolean {
+  if (isFizz(num) && isBuzz(num)) {
+    return true;
+  }
+  return false;
+}
+
+export function asFizzBuzz(num: number): string | number {
+  if (isFizzBuzz(num)) {
+    return "FizzBuzz";
+  } else if (isBuzz(num)) {
+    return "Buzz";
+  } else if (isFizz(num)) {
+    return "Fizz";
+  }
+  return num;
 }
